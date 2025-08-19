e6036a704e5d79d08f22af4bbfbe6b6c40dc1eb1
DZD2023 <121813477+DZD2023@users.noreply.github.com>
2025-08-19 10:01:38 +0200

    Wait for serial connection before printing

--- 03_Firmware/main.ino
+++ 03_Firmware/main.ino
@@ -1,5 +1,8 @@
 void setup() {
   Serial.begin(115200);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB
+  }
   Serial.println("SlopeBot DZ MK1 Test-Upload erfolgreich.");
 }
 
